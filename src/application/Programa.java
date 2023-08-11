819f5d97b7709023c508aeefff67716d7e056c63
wallace13 <wsalesg@hotmail.com>
2023-08-11 19:56:42 -0300

    Player

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -21,7 +21,7 @@ public class Programa {
 		while (true) {
 			try{		
 				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
