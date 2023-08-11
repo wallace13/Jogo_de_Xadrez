85cea1d6df11820eb6aec63002645adcd37d2bd0
wallace13 <wsalesg@hotmail.com>
2023-08-11 17:36:34 -0300

    Movieng Pieces

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,16 +1,35 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new	ChessMatch();
-		UI.printBoard(chessMatch.getPieces());
+		
+		while (true) {
+			UI.printBoard(chessMatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			
+		}
+		
 	}
 
 }
