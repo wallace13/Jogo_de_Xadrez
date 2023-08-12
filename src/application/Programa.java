d71037828cdef377862d01931932707a38a71ca9
wallace13 <wsalesg@hotmail.com>
2023-08-11 21:46:09 -0300

    Check Mate

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -21,7 +21,7 @@ public class Programa {
 		ChessMatch chessMatch = new	ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try{		
 				UI.clearScreen();
 				UI.printMatch(chessMatch,captured);
@@ -53,6 +53,8 @@ public class Programa {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 		
 	}
 
